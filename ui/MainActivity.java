6211a4000d6f53d69119050375c98b79f1763aaa
yuxuan <1057084152@qq.com>
2016-05-03 15:19:52 +0800

    添加BaseActivity，SplashActivity类

---
+++ ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iteam.findu.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.iteam.findu.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
